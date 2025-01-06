ae007f38efd5858a5d516b300da55f0b9630d683
Mauli Dudhat <151643077+mauli-009@users.noreply.github.com>
2025-01-06 22:15:02 +0530

    changes

--- basics/no and maths in js,.js
+++ basics/no and maths in js,.js
@@ -1,3 +1,25 @@
 const score=400;
 const balance=new Number(100)
 console.log(balance) 
+console.log(balance.toString().length)
+console.log(balance.toFixed(2));
+
+const temp=1000000;
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//maths
+
+console.log(Math)
+console.log(Math.abs(-4));
+console.log(Math.round(4.3));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+
+//sqrt ,power ,min,max
+
+console.log(Math.random())
+
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min)
